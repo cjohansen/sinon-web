2ebff5b9f8f545c75873f64f88d8ceb61d73dac1
Christian Johansen <christian@cjohansen.no>
2011-05-04 22:17:28 +0200

    Style updates for frontpage

--- design/js/sinon-web.js
+++ design/js/sinon-web.js
@@ -19,6 +19,7 @@ try {
 } catch (e) {}
 
 (function () {
+    // Examples
     var examples = document.getElementById("examples");
 
     if (!examples) {
@@ -64,4 +65,66 @@ try {
             previousLink = a.parentNode;
         }
     }
+
+/*
+    // Tweets
+    function getTweetsElement(num) {
+	var element = document.getElementById("tweets");
+
+        if (!element) {
+            return null;
+        }
+
+	var heading = document.createElement("h2");
+	heading.innerHTML = num + " tweet" + (num > 1 ? "s" : "");
+	element.appendChild(heading);
+	var tweets = document.createElement("ol");
+	element.appendChild(tweets);
+
+	return tweets;
+    }
+
+    function autoLinkTweeps(text) {
+	return text.replace(/\@([a-zA-Z0-9\-_]+)/g, function (match, user) {
+	    return "<a href=\"http://twitter.com/" + user + "\">" +
+		match + "</a>";
+	});
+    }
+
+    function autoLink(text) {
+	return text.replace(/(http:\/\/[\w\.\d%\/]+)/ig, '<a href="$1">$1</a>');
+    }
+
+    function tweets (json) {
+        var element = document.getElementById("tweets");
+
+        if (!element) {
+            return;
+        }
+
+	var results = json && json.response && json.response.list;
+
+	if (typeof results !== "undefined") {
+	    var length = results.length;
+	    var current, currentAuthor, tweet, tweets = "", link;
+
+	    for (var i = length - 1; i >= 0; i--) {
+		current = results[i];
+		currentAuthor = current.author;
+		link = "<a href=\"" + currentAuthor.url + "\">";
+		tweet = "<li class=\"comment tweet\"><div class=\"vcard\">";
+		tweet += "<h3>" + link + "@" + currentAuthor.nick + "</a> (" + currentAuthor.name + ")</h3>";
+		tweet += "<p><a href=\"" + current.permalink_url + "\">" + current.date_alpha + "</a></p></div>"
+		tweet += "<div class=\"image\">" + link + "<img src=\"" + currentAuthor.photo_url +
+		    "\" alt=\"\" width=\"40\" height=\"40\" /></a></div>"
+		tweet += "<div class=\"entry-content\">" + autoLinkTweeps(autoLink(current.content)) + "</div>";
+		tweets += tweet;
+	    }
+
+	    getTweetsElement(length).innerHTML = tweets + "</ol>";
+	}
+    }
+
+    this.tweets = tweets;
+*/
 }());
